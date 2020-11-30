e28bc7e299df566e912c49a4a097812b153c6fb0
jackmt9 <jackmt9@gmail.com>
2020-11-30 12:45:25 -0500

    added react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navigation'
+import snow from './images/snow.JPG'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={RootContainer} />
+                <Route exact path="/cd-solution" component={CdSolution} />
+            </Switch>
+        </Router>
+    // </Provider>
+    );
+  }
 }
-
 export default App;
